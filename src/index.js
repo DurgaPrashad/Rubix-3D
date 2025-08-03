e22d43f7ec499f2969e9abae95cbced5dac513e9
durga <durgaprashadreddy9966@gmail.com>
2025-08-03 18:45:51 +0530

    added files

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import './index.css';
+
+ReactDOM.render(
+  <div>
+    <App />
+    <a className="github-fork-ribbon"
+     
+      data-ribbon="3*3" title="3*3">3*3</a>
+  </div>,
+  document.getElementById('root')
+);
